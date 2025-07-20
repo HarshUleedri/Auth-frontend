0830ba29089390b8ba6dffb242d13279ba078865
HarshUleedri <ulidari2@gmail.com>
2025-07-20 08:50:33 +0530

    fix: :bug: fixed google auth url

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -33,7 +33,7 @@ const Login = () => {
     e.preventDefault();
     window.location.href = `${
       import.meta.env.VITE_BASE_URL
-    }/api/v1/auth/google`;
+    }/auth/google`;
   };
 
   return (
